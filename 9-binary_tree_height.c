fafae71e5ae6e71298c1153300a4a6af337210b1
Joshua Bubune <joshua.legithive@gmail.com>
2023-11-03 01:11:41 +0000

    Binary tree task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,19 +7,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height = 0;
+	size_t l_height, r_height;
 
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL)
-		height++;
+	l_height = binary_tree_height(tree->left);
+	r_height = binary_tree_height(tree->right);
 
-	if (tree->right != NULL)
-		height++;
-
-	binary_tree_height(tree->left);
-	binary_tree_height(tree->right);
-
-	return (height);
+	return ((l_height > r_height) ? l_height + 1 : r_height + 1);
 }
